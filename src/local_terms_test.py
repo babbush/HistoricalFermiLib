63e6672620c866b3a2eade81b83d58dc760d0fab
idk3 <kivlichan@g.harvard.edu>
2017-01-14 12:52:34 -0500

    Rounding out local_terms_test.

--- src/local_terms_test.py
+++ src/local_terms_test.py
@@ -78,31 +78,78 @@ class LocalTermsTest(unittest.TestCase):
     self.term2 = local_terms.LocalTerm(2, 1)
     with self.assertRaises(TypeError):
       self.term1 + self.term2
-      
-  # TODO Ian: imul, rmul, mul, pow, str tests
-
-  def test_multiplication(self):
+        
+  def test_rmul(self):
+    self.term = self.term_a * -3.
+    expected = local_terms.LocalTerm(self.n_qubits, self.coefficient_a * -3.,
+                                     self.operators_a)
+    self.assertEqual(self.term, expected)
+    
+  def test_lmul_rmul(self):
     new_term = 7. * self.term_a
-    self.assertTrue(3. * new_term == new_term * 3.)
+    self.assertEqual(3. * new_term, new_term * 3.)
     self.assertAlmostEqual(7. * self.term_a.coefficient, new_term.coefficient)
-    self.assertEqual(self.term_a.operators, new_term.operators)
+    self.assertEqual(self.term_a.operators, new_term.operators)    
+    
+  def test_imul_scalar(self):
+    self.term = self.term_a * 1
+    self.term *= -3.+2j
+    expected = local_terms.LocalTerm(self.n_qubits, 
+                                     self.coefficient_a * (-3.+2j),
+                                     self.operators_a)
+    self.assertEqual(self.term, expected)
+  
+  def test_imul_localterm(self):
+    term_ab = self.term_a * self.term_b
+    
+    expected_coeff = self.term_a.coefficient * self.term_b.coefficient
+    expected_ops = self.term_a.operators + self.term_b.operators
+    
+    self.assertEqual(term_ab, 
+                     local_terms.LocalTerm(self.n_qubits,
+                                           expected_coeff, expected_ops))
+    
+  def test_mul_by_zero(self):
+    term1 = self.term_a * 0
+    expected = local_terms.LocalTerm(self.n_qubits, 0, self.term_a.operators)
+    self.assertEqual(term1, expected)
 
-    self.term_a *= 7.
-    self.assertTrue(self.term_a == new_term)
-
+  def test_mul_by_self(self):
     new_term = self.term_a * self.term_a
     self.assertAlmostEqual(self.term_a.coefficient ** 2.,
                            new_term.coefficient)
     self.assertEqual(2 * self.term_a.operators, new_term.operators)
-
+    
+  def test_mul_triple(self):
     new_term = self.term_a * self.term_a * self.term_a
     self.assertAlmostEqual(self.term_a.coefficient ** 3.,
                            new_term.coefficient)
     self.assertEqual(3 * self.term_a.operators, new_term.operators)
     
-  def test_pow(self):
+  def test_pow_square(self):
     squared = self.term_a ** 2
-    self.assertEqual(squared, self.term_a * self.term_a)  
+    self.assertEqual(squared, self.term_a * self.term_a)
+    
+  def test_pow_zero(self):
+    zerod = self.term_a ** 0
+    expected = local_terms.LocalTerm(self.n_qubits, 1.0, [])
+    
+    self.assertEqual(zerod, expected)
+    
+  def test_pow_neg_error(self):
+    with self.assertRaises(ValueError):
+      self.term_a ** -1
+      
+  def test_pow_nonint_error(self):
+    with self.assertRaises(ValueError):
+      self.term_a ** 0.5
+      
+  def test_pow_high(self):
+    high = self.term_a ** 100
+    expected = local_terms.LocalTerm(self.n_qubits, 
+                                     self.term_a.coefficient ** 100,
+                                     self.operators_a * 100)
+    self.assertEqual(high, expected)
 
   def test_iter(self):
     for i, operator in enumerate(self.term_a):
@@ -112,7 +159,18 @@ class LocalTermsTest(unittest.TestCase):
     abs_term_a = abs(self.term_a)
     self.assertAlmostEqual(abs(self.term_a.coefficient),
                            abs_term_a.coefficient)
-
+    
+  def test_abs_complex(self):
+    term1 = local_terms.LocalTerm(3, 2. + 3j, [])
+    self.assertAlmostEqual(abs(term1).coefficient, abs(term1.coefficient))
+  
+  def test_str(self):
+    self.assertEqual(str(self.term_a), "-2.0 [0, 1, 2, 3, 4]")
+    self.assertEqual(str(self.term_b), "8.0 [1, 2]")
+    
+  def test_str_complex(self):
+    term1 = local_terms.LocalTerm(3, -2. - 3j, [])
+    self.assertEqual(str(term1), str(-2. - 3j) + ' ' + str([]))
 
 if __name__ == '__main__':
   unittest.main()
