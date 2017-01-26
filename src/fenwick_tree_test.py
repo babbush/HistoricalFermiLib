5bcd15479a58df925f84043364c4b97d80790b06
Vojtech Havlicek <havlicek@itp.phys.ethz.ch>
2017-01-26 02:25:18 +0000

    Fenwick Tree - proof of concept

--- src/fenwick_tree_test.py
+++ src/fenwick_tree_test.py
@@ -2,19 +2,16 @@ from fenwick_tree import (FenwickTree, FenwickNode)
 import unittest
 
 class FenwickTreeTest(unittest.TestCase):
-    def setUp(self):
-        pass;
+  def setUp(self):
+      pass;
 
-    def test_fenwick_tree_structure(self):
-        """ A lookup test on 5-qubit fenwick tree """ 
+  def test_fenwick_tree_structure(self):
+    """ A lookup test on 5-qubit fenwick tree """ 
         
-        f = FenwickTree(5)
-        
-        self.assertEqual(f.root.children[0].value, 2)
-        self.assertEqual(f.root.children[1].value, 3)
-        self.assertEqual(f.root.children[0].children[0].children[0].value, 0)
-
-
+    f = FenwickTree(5)
+    self.assertEqual(f.root.children[0].value, 2)
+    self.assertEqual(f.root.children[1].value, 3)
+    self.assertEqual(f.root.children[0].children[0].children[0].value, 0)
 
 if __name__ == '__main__':
   unittest.main()       
