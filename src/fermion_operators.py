8d243ebf72c4e7a2e22dae62e82be5a12ba926ac
idk3 <kivlichan@g.harvard.edu>
2017-03-10 16:18:44 -0500

    Changed FermionTerms' str to match init.

--- src/fermion_operators.py
+++ src/fermion_operators.py
@@ -149,7 +149,7 @@ class FermionTerm(LocalTerm):
     """Return an easy-to-read string representation of the term."""
     string_representation = '{} ['.format(self.coefficient)
     for operator in self:
-      string_representation += str(operator[0]) + '+' * operator[1] + ' '
+      string_representation += str(operator[0]) + '^' * operator[1] + ' '
 
     if self:
       string_representation = string_representation[:-1]
