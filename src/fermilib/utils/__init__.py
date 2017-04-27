87ccb50d597e34cb67267882d84adb4393e8341f
Ryan Babbush <babbush@google.com>
2017-04-27 10:16:33 -0700

    updated __init__.py files

--- src/fermilib/utils/__init__.py
+++ src/fermilib/utils/__init__.py
@@ -4,4 +4,11 @@ from ._chemical_series import (make_atomic_ring,
 
 from ._hubbard import fermi_hubbard
 
+from ._jellium import (jellium_model,
+                       jordan_wigner_position_jellium,
+                       momentum_kinetic_operator,
+                       momentum_potential_operator,
+                       position_kinetic_operator,
+                       position_potential_operator)
+
 from ._unitary_cc import uccsd_operator
