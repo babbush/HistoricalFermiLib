03fcb56bc1738072100ce7a6f7fef11aab655376
Ryan Babbush <babbush@google.com>
2017-05-01 14:50:51 -0700

    Broke hydrogen integration into multiple tests. Now moving on to LiH.

--- src/fermilib/tests/_hydrogen_integration_test.py
+++ src/fermilib/tests/_hydrogen_integration_test.py
@@ -10,20 +10,22 @@ from fermilib.ops import *
 from fermilib.transforms import *
 from fermilib.utils import uccsd_operator
 
-_THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+
 def get_test_filename(name):
+  _THIS_DIR = os.path.dirname(os.path.abspath(__file__))
   return os.path.join(_THIS_DIR, name)
+
 
 class HydrogenIntegrationTest(unittest.TestCase):
 
     def setUp(self):
-        # Set up molecule.
         self.geometry = [('H', (0., 0., 0.)), ('H', (0., 0., 0.7414))]
         self.basis = 'sto-3g'
         self.multiplicity = 1
         self.molecule = MolecularData(
             self.geometry, self.basis, self.multiplicity)
-        self.molecule.refresh(filename=get_test_filename("testdata/H2_sto-3g_singlet"))
+        self.molecule.refresh(filename=get_test_filename(
+            "testdata/H2_sto-3g_singlet"))
 
         # Get molecular Hamiltonian.
         self.molecular_hamiltonian = self.molecule.get_molecular_hamiltonian(
@@ -45,145 +47,158 @@ class HydrogenIntegrationTest(unittest.TestCase):
         # Get qubit Hamiltonian.
         self.qubit_hamiltonian = jordan_wigner(self.fermion_hamiltonian)
 
-        # Get matrix m.
+        # Get the sparse matrix.
         self.hamiltonian_matrix = get_sparse_operator(
             self.molecular_hamiltonian)
 
-        # Initialize coefficients given in Seeley and Love paper, arXiv:
-        # 1208.5986.
-        self.g0 = 0.71375
-        self.g1 = -1.2525
-        self.g2 = -0.47593
-        self.g3 = 0.67449 / 2.
-        self.g4 = 0.69740 / 2.
-        self.g5 = 0.66347 / 2.
-        self.g6 = 0.18129 / 2.
+    def test_integral_data(self):
 
-        # Below are qubit term coefficients, also from Seeley and Love.
-        self.f0 = -0.8126
-        self.f1 = 0.1712
-        self.f2 = -0.2228
-        self.f3 = 0.1686
-        self.f4 = 0.1205
-        self.f5 = 0.1659
-        self.f6 = 0.1743
-        self.f7 = 0.04532
+        # Initialize coefficients given in arXiv 1208.5986:
+        g0 = 0.71375
+        g1 = -1.2525
+        g2 = -0.47593
+        g3 = 0.67449 / 2.
+        g4 = 0.69740 / 2.
+        g5 = 0.66347 / 2.
+        g6 = 0.18129 / 2.
 
-    def test_molecular_hydrogen(self):
+        # Check the integrals in the test data.
+        self.assertAlmostEqual(self.nuclear_repulsion, g0, places=4)
 
-        # Check that all the transforms work.
-        qubit_hamiltonian = jordan_wigner(self.fermion_hamiltonian)
-        self.assertTrue(self.qubit_hamiltonian.isclose(qubit_hamiltonian))
+        self.assertAlmostEqual(self.one_body[0, 0], g1, places=4)
+        self.assertAlmostEqual(self.one_body[1, 1], g1, places=4)
 
-        # Check reverse transm.
-        fermion_hamiltonian = reverse_jordan_wigner(qubit_hamiltonian)
+        self.assertAlmostEqual(self.one_body[2, 2], g2, places=4)
+        self.assertAlmostEqual(self.one_body[3, 3], g2, places=4)
+
+        self.assertAlmostEqual(self.two_body[0, 1, 1, 0], g3, places=4)
+        self.assertAlmostEqual(self.two_body[1, 0, 0, 1], g3, places=4)
+
+        self.assertAlmostEqual(self.two_body[2, 3, 3, 2], g4, places=4)
+        self.assertAlmostEqual(self.two_body[3, 2, 2, 3], g4, places=4)
+
+        self.assertAlmostEqual(self.two_body[0, 2, 2, 0], g5, places=4)
+        self.assertAlmostEqual(self.two_body[0, 3, 3, 0], g5, places=4)
+        self.assertAlmostEqual(self.two_body[1, 2, 2, 1], g5, places=4)
+        self.assertAlmostEqual(self.two_body[1, 3, 3, 1], g5, places=4)
+        self.assertAlmostEqual(self.two_body[2, 0, 0, 2], g5, places=4)
+        self.assertAlmostEqual(self.two_body[3, 0, 0, 3], g5, places=4)
+        self.assertAlmostEqual(self.two_body[2, 1, 1, 2], g5, places=4)
+        self.assertAlmostEqual(self.two_body[3, 1, 1, 3], g5, places=4)
+
+        self.assertAlmostEqual(self.two_body[0, 2, 0, 2], g6, places=4)
+        self.assertAlmostEqual(self.two_body[1, 3, 1, 3], g6, places=4)
+        self.assertAlmostEqual(self.two_body[2, 1, 3, 0], g6, places=4)
+        self.assertAlmostEqual(self.two_body[2, 3, 1, 0], g6, places=4)
+        self.assertAlmostEqual(self.two_body[0, 3, 1, 2], g6, places=4)
+        self.assertAlmostEqual(self.two_body[0, 1, 3, 2], g6, places=4)
+
+
+    def test_qubit_operator(self):
+
+        # Below are qubit term coefficients, also from arXiv 1208.5986:
+        f0 = -0.8126
+        f1 = 0.1712
+        f2 = -0.2228
+        f3 = 0.1686
+        f4 = 0.1205
+        f5 = 0.1659
+        f6 = 0.1743
+        f7 = 0.04532
+
+        # Test the local Hamiltonian terms.
+        self.assertAlmostEqual(
+            self.qubit_hamiltonian.terms[((0, 'Z'),)], f1, places=4)
+        self.assertAlmostEqual(
+            self.qubit_hamiltonian.terms[((1, 'Z'),)], f1, places=4)
+
+        self.assertAlmostEqual(
+            self.qubit_hamiltonian.terms[((2, 'Z'),)], f2, places=4)
+        self.assertAlmostEqual(
+            self.qubit_hamiltonian.terms[((3, 'Z'),)], f2, places=4)
+
+        self.assertAlmostEqual(
+            self.qubit_hamiltonian.terms[((0, 'Z'), (1, 'Z'))],
+            f3, places=4)
+
+        self.assertAlmostEqual(
+            self.qubit_hamiltonian.terms[((0, 'Z'), (2, 'Z'))],
+            f4, places=4)
+        self.assertAlmostEqual(
+            self.qubit_hamiltonian.terms[((1, 'Z'), (3, 'Z'))],
+            f4, places=4)
+
+        self.assertAlmostEqual(
+            self.qubit_hamiltonian.terms[((1, 'Z'), (2, 'Z'))],
+            f5, places=4)
+        self.assertAlmostEqual(
+            self.qubit_hamiltonian.terms[((0, 'Z'), (3, 'Z'))],
+            f5, places=4)
+
+        self.assertAlmostEqual(
+            self.qubit_hamiltonian.terms[((2, 'Z'), (3, 'Z'))],
+            f6, places=4)
+
+        self.assertAlmostEqual(
+            self.qubit_hamiltonian.terms[((0, 'Y'), (1, 'Y'),
+                                          (2, 'X'), (3, 'X'))],
+            -f7, places=4)
+        self.assertAlmostEqual(
+            self.qubit_hamiltonian.terms[((0, 'X'), (1, 'X'),
+                                          (2, 'Y'), (3, 'Y'))],
+            -f7, places=4)
+
+        self.assertAlmostEqual(
+            self.qubit_hamiltonian.terms[((0, 'X'), (1, 'Y'),
+                                          (2, 'Y'), (3, 'X'))],
+            f7, places=4)
+        self.assertAlmostEqual(
+            self.qubit_hamiltonian.terms[((0, 'Y'), (1, 'X'),
+                                          (2, 'X'), (3, 'Y'))],
+            f7, places=4)
+
+    def test_reverse_jordan_wigner(self):
+        fermion_hamiltonian = reverse_jordan_wigner(self.qubit_hamiltonian)
         fermion_hamiltonian = normal_ordered(fermion_hamiltonian)
         self.assertTrue(self.fermion_hamiltonian.isclose(fermion_hamiltonian))
 
-        # Make sure the mapping of FermionOperator to MolecularOperator works.
+    def test_interaction_operator_mapping(self):
+
+        # Make sure the mapping of FermionOperator to InteractionOperator works.
         molecular_hamiltonian = get_interaction_operator(
             self.fermion_hamiltonian)
         fermion_hamiltonian = get_fermion_operator(molecular_hamiltonian)
         self.assertTrue(self.fermion_hamiltonian.isclose(fermion_hamiltonian))
 
-        # Make sure mapping of MolecularOperator to QubitOperator works.
+        # Make sure mapping of InteractionOperator to QubitOperator works.
         qubit_hamiltonian = jordan_wigner(self.molecular_hamiltonian)
         self.assertTrue(self.qubit_hamiltonian.isclose(qubit_hamiltonian))
 
-        # Check that FCI prior has the correct energy.
+    def test_rdm_numerically(self):
+
+        # Test energy of RDM.
         fci_rdm_energy = self.nuclear_repulsion
         fci_rdm_energy += numpy.sum(self.fci_rdm.one_body_tensor *
-                                    molecular_hamiltonian.one_body_tensor)
+                                    self.molecular_hamiltonian.one_body_tensor)
         fci_rdm_energy += numpy.sum(self.fci_rdm.two_body_tensor *
-                                    molecular_hamiltonian.two_body_tensor)
+                                    self.molecular_hamiltonian.two_body_tensor)
         self.assertAlmostEqual(fci_rdm_energy, self.molecule.fci_energy)
 
-        # Check the integrals.
-        self.assertAlmostEqual(self.nuclear_repulsion, self.g0, places=4)
+        # Confirm expectation on qubit Hamiltonian using reverse JW matches.
+        qubit_rdm = self.fci_rdm.get_qubit_expectations(self.qubit_hamiltonian)
+        qubit_energy = self.qubit_hamiltonian.terms[()]
+        for term, expectation in qubit_rdm.terms.items():
+          qubit_energy += expectation * self.qubit_hamiltonian.terms[term]
+        self.assertAlmostEqual(qubit_energy, self.molecule.fci_energy)
 
-        self.assertAlmostEqual(self.one_body[0, 0], self.g1, places=4)
-        self.assertAlmostEqual(self.one_body[1, 1], self.g1, places=4)
+        # Confirm fermionic RDMs can be built from measured qubit RDMs
+        new_fermi_rdm = get_interaction_rdm(qubit_rdm)
+        new_fermi_rdm.expectation(self.molecular_hamiltonian)
+        self.assertAlmostEqual(fci_rdm_energy, self.molecule.fci_energy)
 
-        self.assertAlmostEqual(self.one_body[2, 2], self.g2, places=4)
-        self.assertAlmostEqual(self.one_body[3, 3], self.g2, places=4)
+    def test_sparse_numerically(self):
 
-        self.assertAlmostEqual(self.two_body[0, 1, 1, 0], self.g3, places=4)
-        self.assertAlmostEqual(self.two_body[1, 0, 0, 1], self.g3, places=4)
-
-        self.assertAlmostEqual(self.two_body[2, 3, 3, 2], self.g4, places=4)
-        self.assertAlmostEqual(self.two_body[3, 2, 2, 3], self.g4, places=4)
-
-        self.assertAlmostEqual(self.two_body[0, 2, 2, 0], self.g5, places=4)
-        self.assertAlmostEqual(self.two_body[0, 3, 3, 0], self.g5, places=4)
-        self.assertAlmostEqual(self.two_body[1, 2, 2, 1], self.g5, places=4)
-        self.assertAlmostEqual(self.two_body[1, 3, 3, 1], self.g5, places=4)
-        self.assertAlmostEqual(self.two_body[2, 0, 0, 2], self.g5, places=4)
-        self.assertAlmostEqual(self.two_body[3, 0, 0, 3], self.g5, places=4)
-        self.assertAlmostEqual(self.two_body[2, 1, 1, 2], self.g5, places=4)
-        self.assertAlmostEqual(self.two_body[3, 1, 1, 3], self.g5, places=4)
-
-        self.assertAlmostEqual(self.two_body[0, 2, 0, 2], self.g6, places=4)
-        self.assertAlmostEqual(self.two_body[1, 3, 1, 3], self.g6, places=4)
-        self.assertAlmostEqual(self.two_body[2, 1, 3, 0], self.g6, places=4)
-        self.assertAlmostEqual(self.two_body[2, 3, 1, 0], self.g6, places=4)
-        self.assertAlmostEqual(self.two_body[0, 3, 1, 2], self.g6, places=4)
-        self.assertAlmostEqual(self.two_body[0, 1, 3, 2], self.g6, places=4)
-
-        # Make sure its actually normal ordered.
-        self.assertTrue(self.fermion_hamiltonian.is_normal_ordered())
-
-        # Test the local Hamiltonian terms.
-        self.assertAlmostEqual(
-            self.qubit_hamiltonian.terms[((0, 'Z'),)], self.f1, places=4)
-        self.assertAlmostEqual(
-            self.qubit_hamiltonian.terms[((1, 'Z'),)], self.f1, places=4)
-
-        self.assertAlmostEqual(
-            self.qubit_hamiltonian.terms[((2, 'Z'),)], self.f2, places=4)
-        self.assertAlmostEqual(
-            self.qubit_hamiltonian.terms[((3, 'Z'),)], self.f2, places=4)
-
-        self.assertAlmostEqual(
-            self.qubit_hamiltonian.terms[((0, 'Z'), (1, 'Z'))],
-            self.f3, places=4)
-
-        self.assertAlmostEqual(
-            self.qubit_hamiltonian.terms[((0, 'Z'), (2, 'Z'))],
-            self.f4, places=4)
-        self.assertAlmostEqual(
-            self.qubit_hamiltonian.terms[((1, 'Z'), (3, 'Z'))],
-            self.f4, places=4)
-
-        self.assertAlmostEqual(
-            self.qubit_hamiltonian.terms[((1, 'Z'), (2, 'Z'))],
-            self.f5, places=4)
-        self.assertAlmostEqual(
-            self.qubit_hamiltonian.terms[((0, 'Z'), (3, 'Z'))],
-            self.f5, places=4)
-
-        self.assertAlmostEqual(
-            self.qubit_hamiltonian.terms[((2, 'Z'), (3, 'Z'))],
-            self.f6, places=4)
-
-        self.assertAlmostEqual(
-            self.qubit_hamiltonian.terms[((0, 'Y'), (1, 'Y'),
-                                          (2, 'X'), (3, 'X'))],
-            -self.f7, places=4)
-        self.assertAlmostEqual(
-            self.qubit_hamiltonian.terms[((0, 'X'), (1, 'X'),
-                                          (2, 'Y'), (3, 'Y'))],
-            -self.f7, places=4)
-
-        self.assertAlmostEqual(
-            self.qubit_hamiltonian.terms[((0, 'X'), (1, 'Y'),
-                                          (2, 'Y'), (3, 'X'))],
-            self.f7, places=4)
-        self.assertAlmostEqual(
-            self.qubit_hamiltonian.terms[((0, 'Y'), (1, 'X'),
-                                          (2, 'X'), (3, 'Y'))],
-            self.f7, places=4)
-
-        # Test the matrix representation.
+        # Check FCI energy.
         energy, wavefunction = self.hamiltonian_matrix.get_ground_state()
         self.assertAlmostEqual(energy, self.molecule.fci_energy)
         expected_energy = self.hamiltonian_matrix.expectation(wavefunction)
@@ -205,23 +220,15 @@ class HydrogenIntegrationTest(unittest.TestCase):
         self.assertAlmostEqual(expected_hf_density_energy,
                                self.molecule.hf_energy)
 
-        # Confirm expectation on qubit Hamiltonian using reverse JW matches.
-        qubit_rdm = self.fci_rdm.get_qubit_expectations(self.qubit_hamiltonian)
-        qubit_energy = self.qubit_hamiltonian.terms[()]
-        for term, expectation in qubit_rdm.terms.items():
-          qubit_energy += expectation * self.qubit_hamiltonian.terms[term]
-        self.assertAlmostEqual(qubit_energy, self.molecule.fci_energy)
-
-        # Confirm fermionic RDMs can be built from measured qubit RDMs
-        new_fermi_rdm = get_interaction_rdm(qubit_rdm)
-        new_fermi_rdm.expectation(self.molecular_hamiltonian)
-        self.assertAlmostEqual(fci_rdm_energy, self.molecule.fci_energy)
+    def test_ucc(self):
 
         # Test UCCSD for accuracy against FCI using loaded t amplitudes.
         ucc_operator = uccsd_operator(
             self.molecule.ccsd_amplitudes.one_body_tensor,
             self.molecule.ccsd_amplitudes.two_body_tensor)
 
+        hf_state = jw_hartree_fock_state(
+            self.molecule.n_electrons, self.qubit_hamiltonian.n_qubits())
         uccsd_sparse = jordan_wigner_operator_sparse(
             ucc_operator, self.qubit_hamiltonian.n_qubits())
         uccsd_state = scipy.sparse.linalg.expm_multiply(uccsd_sparse.matrix,
