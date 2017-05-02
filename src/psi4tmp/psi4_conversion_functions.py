f5899578611ddad522f5bd1d11c1b3b172865770
Ryan Babbush <babbush@google.com>
2017-05-02 14:58:20 -0700

    Moved unpack_spatial_rdm to psi4_conversion_functions.py

--- src/psi4tmp/psi4_conversion_functions.py
+++ src/psi4tmp/psi4_conversion_functions.py
@@ -6,6 +6,78 @@ import numpy
 from fermilib.ops import InteractionOperator
 
 
+def unpack_spatial_rdm(one_rdm_a,
+                       one_rdm_b,
+                       two_rdm_aa,
+                       two_rdm_ab,
+                       two_rdm_bb):
+    """
+    Covert from spin compact spatial format to spin-orbital format for RDM.
+
+    Note: the compact 2-RDM is stored as follows where A/B are spin up/down:
+    RDM[pqrs] = <| a_{p, A}^\dagger a_{r, A}^\dagger a_{q, A} a_{s, A} |>
+      for 'AA'/'BB' spins.
+    RDM[pqrs] = <| a_{p, A}^\dagger a_{r, B}^\dagger a_{q, B} a_{s, A} |>
+      for 'AB' spins.
+
+    Args:
+        one_rdm_a: 2-index numpy array storing alpha spin
+            sector of 1-electron reduced density matrix.
+        one_rdm_b: 2-index numpy array storing beta spin
+            sector of 1-electron reduced density matrix.
+        two_rdm_aa: 4-index numpy array storing alpha-alpha spin
+            sector of 2-electron reduced density matrix.
+        two_rdm_ab: 4-index numpy array storing alpha-beta spin
+            sector of 2-electron reduced density matrix.
+        two_rdm_bb: 4-index numpy array storing beta-beta spin
+            sector of 2-electron reduced density matrix.
+
+    Returns:
+        one_rdm: 2-index numpy array storing 1-electron density matrix
+            in full spin-orbital space.
+        two_rdm: 4-index numpy array storing 2-electron density matrix
+            in full spin-orbital space.
+    """
+    # Initialize RDMs.
+    n_orbitals = one_rdm_a.shape[0]
+    n_qubits = 2 * n_orbitals
+    one_rdm = numpy.zeros((n_qubits, n_qubits))
+    two_rdm = numpy.zeros((n_qubits, n_qubits,
+                           n_qubits, n_qubits))
+
+    # Unpack compact representation.
+    for p in range(n_orbitals):
+        for q in range(n_orbitals):
+
+            # Populate 1-RDM.
+            one_rdm[2 * p, 2 * q] = one_rdm_a[p, q]
+            one_rdm[2 * p + 1, 2 * q + 1] = one_rdm_b[p, q]
+
+            # Continue looping to unpack 2-RDM.
+            for r in range(n_orbitals):
+                for s in range(n_orbitals):
+
+                    # Handle case of same spin.
+                    two_rdm[2 * p, 2 * q, 2 * r, 2 * s] = (
+                        two_rdm_aa[p, r, q, s])
+                    two_rdm[2 * p + 1, 2 * q + 1, 2 * r + 1, 2 * s + 1] = (
+                        two_rdm_bb[p, r, q, s])
+
+                    # Handle case of mixed spin.
+                    two_rdm[2 * p, 2 * q + 1, 2 * r, 2 * s + 1] = (
+                        two_rdm_ab[p, r, q, s])
+                    two_rdm[2 * p, 2 * q + 1, 2 * r + 1, 2 * s] = (
+                        -1. * two_rdm_ab[p, s, q, r])
+                    two_rdm[2 * p + 1, 2 * q, 2 * r + 1, 2 * s] = (
+                        two_rdm_ab[q, s, p, r])
+                    two_rdm[2 * p + 1, 2 * q, 2 * r, 2 * s + 1] = (
+                        -1. * two_rdm_ab[q, r, p, s])
+
+    # Map to physicist notation and return.
+    two_rdm = numpy.einsum('pqsr', two_rdm)
+    return one_rdm, two_rdm
+
+
 def parse_psi4_ccsd_amplitudes(number_orbitals,
                                n_alpha_electrons, n_beta_electrons,
                                psi_filename):
