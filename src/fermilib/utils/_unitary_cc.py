72a8543141325e57cd4d25d0473819e1b245c8a0
Jarrod McClean <jmcclean@lbl.gov>
2017-05-05 15:07:16 -0700

    Readded updated unitary CC

--- src/fermilib/utils/_unitary_cc.py
+++ src/fermilib/utils/_unitary_cc.py
@@ -13,24 +13,30 @@
 """Module to create and manipulate unitary coupled cluster operators."""
 from __future__ import absolute_import
 
-import copy
 import itertools
+
+import numpy
 import projectq
-import projectq.setups
-import projectq.setups.decompositions
-import projectq.ops
 import projectq.backends
 import projectq.cengines
 import projectq.meta
-
+import projectq.ops
+import projectq.setups
+import projectq.setups.decompositions
 from fermilib.ops import FermionOperator
 from fermilib.transforms import jordan_wigner
 
-import numpy
 
 def uccsd_operator(single_amplitudes, double_amplitudes, anti_hermitian=True):
-    """
-    Create a fermionic operator that is the generator of uccsd.
+    """Create a fermionic operator that is the generator of uccsd.
+
+    This a the most straight-forward method to generate UCCSD operators,
+    however it is slightly inefficient.  In particular, it parameterizes
+    all possible exictations, so it represents a generalized unitary coupled
+    cluster ansatz, but also does not explicitly enforce the uniqueness
+    in paramterization, so it is redundant. For example there will be a linear
+    dependency in the ansatz of single_amplitudes[i,j] and
+    single_amplitudes[j,i].
 
     Args:
         single_amplitudes(ndarray): [NxN] array storing single excitation
@@ -75,80 +81,154 @@ def uccsd_operator(single_amplitudes, double_amplitudes, anti_hermitian=True):
 
 
 def uccsd_singlet_paramsize(n_qubits, n_electrons):
-    N = n_qubits
-    n_occ = int(numpy.ceil(n_electrons / 2.))
-    n_virt = N / 2 - n_occ  # Virtual Spatial Orbitals
-    n_t1 = n_occ * n_virt
-    n_t2 = n_t1 ** 2
-    return n_t1 + n_t2
+    """Determine number of independent amplitudes for singlet UCCSD
+
+    Args:
+        n_qubits(int): Number of qubits/spin-orbitals in the system
+        n_electrons(int): Number of electrons in the reference state
+
+    Returns:
+        Number of independent parameters for singlet UCCSD with a single
+        reference.
+    """
+    n_occupied = int(numpy.ceil(n_electrons / 2.))
+    n_virtual = n_qubits / 2 - n_occupied
+
+    n_single_amplitudes = n_occupied * n_virtual
+    n_double_amplitudes = n_single_amplitudes ** 2
+    return (n_single_amplitudes + n_double_amplitudes)
+
 
 def uccsd_singlet_operator(packed_amplitudes,
                            n_qubits,
-                           n_electrons,
-                           anti_hermitian=True):
-    """ """
-    N = n_qubits
-    n_occ = int(numpy.ceil(n_electrons / 2.))
-    n_virt = N / 2 - n_occ  # Virtual Spatial Orbitals
-    n_t1 = n_occ * n_virt
+                           n_electrons):
+    """Create a singlet UCCSD generator for a system with n_electrons
+
+    This function generates a FermionOperator for a UCCSD generator designed
+        to act on a single reference state consisting of n_qubits spin orbitals
+        and n_electrons electrons, that is a spin singlet operator, meaning it
+        conserves spin.
+
+    Args:
+        packed_amplitudes(ndarray): Compact array storing the unique single
+            and double excitation amplitudes for a singlet UCCSD operator.
+            The ordering lists unique single excitations before double
+            excitations.
+        n_qubits(int): Number of spin-orbitals used to represent the system,
+            which also corresponds to number of qubits in a non-compact map.
+        n_electrons(int): Number of electrons in the physical system.
+
+    Returns:
+        uccsd_generator(FermionOperator): Generator of the UCCSD operator that
+            builds the UCCSD wavefunction.
+
+    """
+    n_occupied = int(numpy.ceil(n_electrons / 2.))
+    n_virtual = n_qubits / 2 - n_occupied  # Virtual Spatial Orbitals
+    n_t1 = n_occupied * n_virtual
     n_t2 = n_t1 ** 2
 
     t1 = packed_amplitudes[:n_t1]
     t2 = packed_amplitudes[n_t1:]
 
-    t1_ind = lambda i, j: i * n_occ + j
-    t2_ind = lambda i, j, k, l: i * n_occ * n_virt * n_occ \
-                                + j * n_virt * n_occ \
-                                + k * n_occ \
-                                + l
-    uccsd_generator = 0* FermionOperator()
+    def t1_ind(i, j):
+        return i * n_occupied + j
 
-    for i in range(n_virt):
-        for j in range(n_occ):
+    def t2_ind(i, j, k, l):
+        return i * n_occupied * n_virtual * n_occupied \
+            + j * n_virtual * n_occupied \
+            + k * n_occupied \
+            + l
+
+    uccsd_generator = FermionOperator()
+
+    for i in range(n_virtual):
+        for j in range(n_occupied):
             for s1 in range(2):
-                uccsd_generator += FermionOperator(
-                    (
-                    (2 * (i + n_occ) + s1, 1),
+                uccsd_generator += FermionOperator((
+                    (2 * (i + n_occupied) + s1, 1),
                     (2 * j + s1, 0)),
-                    t1[t1_ind(i, j)] )
+                    t1[t1_ind(i, j)])
 
-                if (anti_hermitian):
-                    uccsd_generator += FermionOperator(
-                        (
-                        (2 * j + s1, 1),
-                        (2 * (i + n_occ) + s1, 0)),
-                        -t1[t1_ind(i, j)] )
+                uccsd_generator += FermionOperator((
+                    (2 * j + s1, 1),
+                    (2 * (i + n_occupied) + s1, 0)),
+                    -t1[t1_ind(i, j)])
 
-    for i in range(n_virt):
-        for j in range(n_occ):
+    for i in range(n_virtual):
+        for j in range(n_occupied):
             for s1 in range(2):
-                for k in range(n_virt):
-                    for l in range(n_occ):
+                for k in range(n_virtual):
+                    for l in range(n_occupied):
                         for s2 in range(2):
-                            uccsd_generator += FermionOperator(
-                                (
-                                (2 * (i + n_occ) + s1, 1),
+                            uccsd_generator += FermionOperator((
+                                (2 * (i + n_occupied) + s1, 1),
                                 (2 * j + s1, 0),
-                                (2 * (k + n_occ) + s2, 1),
+                                (2 * (k + n_occupied) + s2, 1),
                                 (2 * l + s2, 0)),
                                 t2[t2_ind(i, j, k, l)])
 
-                            if (anti_hermitian):
-                                uccsd_generator += FermionOperator(
-                                    (
-                                    (2 * l + s2, 1),
-                                    (2 * (k + n_occ) + s2, 0),
-                                    (2 * j + s1, 1),
-                                    (2 * (i + n_occ) + s1, 0)),
-                                    -t2[t2_ind(i, j, k, l)])
-
+                            uccsd_generator += FermionOperator((
+                                (2 * l + s2, 1),
+                                (2 * (k + n_occupied) + s2, 0),
+                                (2 * j + s1, 1),
+                                (2 * (i + n_occupied) + s1, 0)),
+                                -t2[t2_ind(i, j, k, l)])
     return uccsd_generator
 
 
-def _identify_non_commuting(cmd):
+def uccsd_singlet_evolution(packed_amplitudes, n_qubits, n_electrons,
+                            fermion_transform=jordan_wigner):
+    """Create a ProjectQ evolution operator for a UCCSD singlet circuit
+
+    Args:
+        packed_amplitudes(ndarray): Compact array storing the unique single
+            and double excitation amplitudes for a singlet UCCSD operator.
+            The ordering lists unique single excitations before double
+            excitations.
+        n_qubits(int): Number of spin-orbitals used to represent the system,
+            which also corresponds to number of qubits in a non-compact map.
+        n_electrons(int): Number of electrons in the physical system
+        fermion_transform(fermilib.transform): The transformation that
+            defines the mapping from Fermions to QubitOperator.
+
+    Returns:
+        evoution_operator(projectq.ops.TimeEvolution): The unitary operator
+            that constructs the UCCSD singlet state.
     """
-    Recognize all TimeEvolution gates with >1 terms but which don't all
-    commute.
+
+    # Build UCCSD generator
+    fermion_generator = uccsd_singlet_operator(packed_amplitudes,
+                                               n_qubits,
+                                               n_electrons)
+    # Transform generator to qubits
+    qubit_generator = fermion_transform(fermion_generator)
+
+    # Cast to real part only for compatibility with current ProjectQ routine
+    for key in qubit_generator.terms:
+        qubit_generator.terms[key] = float(qubit_generator.terms[key].imag)
+    qubit_generator.compress()
+
+    # Allocate wavefunction and act evolution on gate according to compilation
+    evolution_operator = projectq.ops.\
+        TimeEvolution(time=1.,
+                      hamiltonian=qubit_generator)
+
+    return evolution_operator
+
+
+def _identify_non_commuting(cmd):
+    """Recognize all TimeEvolution gates with >1 terms that don't all commute.
+
+    This is a filter function for use with ProjectQ that flags terms as
+        non-commuting so they may be handled by a different factorization
+        routine.
+
+    Args:
+        cmd(projectq.command): A command from ProjectQ
+
+    Returns:
+        (bool) Depending on whether the terms are determined to commute or not
     """
     hamiltonian = cmd.gate.hamiltonian
     if len(hamiltonian.terms) == 1:
@@ -158,7 +238,8 @@ def _identify_non_commuting(cmd):
         for term in hamiltonian.terms:
             test_op = projectq.ops.QubitOperator(term, hamiltonian.terms[term])
             for other in hamiltonian.terms:
-                other_op = projectq.ops.QubitOperator(other, hamiltonian.terms[other])
+                other_op = projectq.\
+                    ops.QubitOperator(other, hamiltonian.terms[other])
                 commutator = test_op * other_op - other_op * test_op
                 if not commutator.isclose(id_op,
                                           rel_tol=1e-9,
@@ -166,7 +247,17 @@ def _identify_non_commuting(cmd):
                     return True
     return False
 
+
 def _first_order_trotter(cmd):
+    """Define a Trotter splitting for non-commuting Pauli in ProjectQ
+
+    This routine defines a first-order Trotter splitting to be applied to
+    time evolution operators in ProjectQ.
+
+    Args:
+        cmd(projectq.command): A command from ProjectQ that needs to be
+            factorized due to non-commuting terms
+    """
     qureg = cmd.qubits
     eng = cmd.engine
     hamiltonian = cmd.gate.hamiltonian
@@ -174,21 +265,52 @@ def _first_order_trotter(cmd):
     with projectq.meta.Control(eng, cmd.control_qubits):
         # First order Trotter splitting
             for term in hamiltonian.terms:
-                ind_operator = projectq.ops.QubitOperator(term, hamiltonian.terms[term])
+                ind_operator = projectq.\
+                    ops.QubitOperator(term, hamiltonian.terms[term])
                 projectq.ops.TimeEvolution(time, ind_operator) | qureg
 
+
 def _two_gate_filter(self, cmd):
+    """A ProjectQ filter to flag TimeEvolution operators for decomposition
+
+    This flags any gates which act on more than 2 qubits or are time evolution
+    operators to be decomposed into a base library of gates for simulation
+    within ProjectQ.
+
+    Args:
+        self(Dummy): Dummy parameter to meet function specification.
+        cmd(projectq.command): Command to be checked for decomposition into
+            one- and two- qubit gates.
+
+    """
     if ((not isinstance(cmd.gate, projectq.ops.TimeEvolution)) and
         (len(cmd.qubits[0]) <= 2 or
-             isinstance(cmd.gate, projectq.ops.ClassicalInstructionGate))):
+            isinstance(cmd.gate, projectq.ops.ClassicalInstructionGate))):
         return True
     return False
 
+
 def uccsd_trotter_engine(compiler_backend=projectq.backends.Simulator()):
-    # Set ProjectQ rules to two-qubit gates with first order Trotter
+    """Define a ProjectQ compiler engine that is common for use with UCCSD
+
+    This defines a ProjectQ compiler engine that decomposes time evolution
+    gates using a first order Trotter decomposition on non-commuting gates
+    down to a base gate decomposition.
+
+    Args:
+        compiler_backend(projectq.backend): Define the backend on the
+            circuit compiler, so that it may either simulate gates numerically
+            or alternatively print a gate sequence, e.g. using
+            projectq.backends.CommandPrinter()
+
+    Returns:
+        projectq.cengine that is the compiler engine set up with these
+            rules and decompostions.
+    """
+
     rule_set = \
         projectq.cengines. \
-            DecompositionRuleSet(modules=[projectq.setups.decompositions])
+        DecompositionRuleSet(modules=[projectq.setups.decompositions])
     trotter_rule_set = projectq.cengines. \
         DecompositionRule(gate_class=projectq.ops.TimeEvolution,
                           gate_decomposer=_first_order_trotter,
@@ -197,33 +319,10 @@ def uccsd_trotter_engine(compiler_backend=projectq.backends.Simulator()):
     replacer = projectq.cengines.AutoReplacer(rule_set)
 
     # Start the compiler engine with these rules
-    compiler_engine = projectq.MainEngine(backend=compiler_backend,
-                                          engine_list=[replacer,
-                                                       projectq.cengines.
-                                          InstructionFilter(_two_gate_filter)])
+    compiler_engine = projectq.\
+        MainEngine(backend=compiler_backend,
+                   engine_list=[replacer,
+                                projectq.
+                                cengines.InstructionFilter(_two_gate_filter),
+                                projectq.cengines.LocalOptimizer(3)])
     return compiler_engine
-
-def uccsd_circuit(packed_amplitudes, n_qubits, n_electrons,
-                  fermion_transform=jordan_wigner):
-    """Create a uccsd ansatz circuit"""
-
-    # Build UCCSD generator
-    #fermion_generator = uccsd_operator(packed_amplitudes[0],
-    #                                   packed_amplitudes[1])
-    fermion_generator = uccsd_singlet_operator(packed_amplitudes,
-                                               n_qubits,
-                                               n_electrons)
-    # Transform generator to qubits
-    qubit_generator = fermion_transform(fermion_generator)
-
-    # Cast to real part only for compatibility with current ProjectQ routine
-    for key in qubit_generator.terms:
-        qubit_generator.terms[key] = -float(qubit_generator.terms[key].imag)
-
-
-    # Allocate wavefunction and act evolution on gate according to compilation
-    evolution_operator = projectq.ops.\
-        TimeEvolution(time=1.,
-                      hamiltonian=qubit_generator)
-
-    return evolution_operator
