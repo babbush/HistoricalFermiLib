d3862ef8ac64fd099b006e34b26a7abe2cdca3c2
Thomas Haener <thomas_haener@hotmail.com>
2017-05-07 22:54:54 -0700

    Updated setup for README.rst

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ from setuptools import setup, find_packages
 # This reads the __version__ variable from projectq/_version.py
 exec(open('src/fermilib/_version.py').read())
 # Readme file as long_description:
-long_description = open('README').read()
+long_description = open('README.rst').read()
 # Read in requirements.txt
 requirements = open('requirements.txt').readlines()
 requirements = [r.strip() for r in requirements]
