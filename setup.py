eef49bcbc81183a7fed89af1dd08b1acdf3610ba
Ryan Babbush <ryanbabbush@Ryans-MacBook-Pro.local>
2017-05-07 17:56:46 -0700

    unbroke setup.py

--- setup.py
+++ setup.py
@@ -12,14 +12,14 @@ requirements = [r.strip() for r in requirements]
 setup(
     name='fermilib',
     version=__version__,
-    author='Ryan Babbush, Jarrod McClean, Damian Steiger, Ian Kivlichan,
-    Thomas Haener, Vojtech Havlicek, Matthew Neeley, Wei Sun'
-    author_email='ryanbabbush@gmail.com, jarrod.mcc@gmail.com,
-    fermilib@projectq.ch',
+    author='Ryan Babbush, Jarrod McClean, Damian Steiger, Ian Kivlichan, '
+    'Thomas Haener, Vojtech Havlicek, Matthew Neeley, Wei Sun',
+    author_email='ryanbabbush@gmail.com, jarrod.mcc@gmail.com, '
+    'fermilib@projectq.ch',
     url='http://www.projectq.ch',
     description=('FermiLib - '
-                 'An open source package for analyzing, compiling and
-                 emulating quantum algorithms for simulation of fermions.'),
+                 'An open source package for analyzing, compiling and '
+                 'emulating quantum algorithms for simulation of fermions.'),
     long_description=long_description,
     install_requires=requirements,
     license='Apache 2',
