e9858f4272a31840e8322ac53f14ae007169767c
Thomas Haener <thomas_haener@hotmail.com>
2017-05-07 22:13:15 -0700

    Last pep8 violation

--- src/fermilib/ops/_fermion_operator.py
+++ src/fermilib/ops/_fermion_operator.py
@@ -175,7 +175,8 @@ class FermionOperator(object):
         Example:
             .. code-block:: python
 
-                ham = (FermionOperator('0^ 3', .5) + .5 * FermionOperator('3^ 0'))
+                ham = (FermionOperator('0^ 3', .5)
+                       + .5 * FermionOperator('3^ 0'))
                 # Equivalently
                 ham2 = FermionOperator('0^ 3', 0.5)
                 ham2 += FermionOperator('3^ 0', 0.5)
